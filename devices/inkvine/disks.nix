ece2f2d54b831cb6e64a013b400535dde9c3cc80
Alaro <glooriouswalrus@protonmail.com>
2024-11-25 03:25:41 +0200

    Update disks.nix

--- devices/inkvine/disks.nix
+++ devices/inkvine/disks.nix
@@ -1,7 +1,7 @@
 { ... }: {
   device.disks = {
-    boot = "5BD3-FDE6";
+    boot = "1B27-75C7";
     crypt = "7171d4c2-fd27-45ed-b058-31bab14681e8";
-    root = "f2b17157-1e79-4a44-8349-e611c4768949";
+    root = "d8a8be0a-a3e3-45dd-bd80-ea421322dbbc";
   };
 }
